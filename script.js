5d7226501a42c9603784c34016be6b9d307e0e0a
saif-saleem <133539527+saif-saleem@users.noreply.github.com>
2023-09-19 21:03:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function volume_sphere() {
     //Write your code here
+	let btn=document.getElementById("submit");
+	let r=document.getElementById("radius");
+	let vol=document.getElementById("volume");
+	btn.addEventListener("click",()=>{
+		let ra=r.value;
+		let v=(4*22/7*ra*ra*ra)/3;
+		vol.value=v;
+	})
   
 } 
 
